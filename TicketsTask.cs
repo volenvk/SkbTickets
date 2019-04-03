dbef32423ea5c0eda48e9b9330c5beac89fd4105
ValeriyKov <valeriy.kov@hotmail.com>
2019-04-03 16:04:58 +0500

    Initial commit Task complited

---
+++ TicketsTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets
+{
+    public static class TicketsTask
+    {
+        private static Dictionary<string,BigInteger> dic;
+        
+        public static BigInteger Solve(int totalLen, int totalSum)
+        {
+            if (totalSum % 2 == 1) return 0;
+            dic = new Dictionary<string,BigInteger>();
+            var result = GetTicketCount(totalLen, totalSum / 2);
+            return result * result;
+        }
+
+        private static BigInteger GetTicketCount(int v, int sum)
+        {
+            var key = $"{v},{sum}";
+            if (sum < 0) return 0;
+
+            if (dic.ContainsKey(key)) return dic[key];
+            
+            switch (v)
+            {
+                case 0 when sum == 0:
+                    dic[key] = 1;
+                    return 1;
+                case 0 when sum != 0:
+                    dic[key] = 0;
+                    return 0;
+            }
+
+            BigInteger ans = 0;
+
+            for (var i = 0; i < 10; i++)
+                ans += GetTicketCount(v - 1, sum - i);
+
+            dic[key] = ans;
+
+            return ans;
+        }
+    }
+}
---
+++ TicketsTask_should.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets
+{
+	[TestFixture]
+    public class TicketsTask_should
+    {
+        public void MakeTest(int totalLen, int totalSum, string answer)
+        {
+            var result = BigInteger.Parse(answer);
+            Assert.AreEqual(result, TicketsTask.Solve(totalLen, totalSum));
+        }
+
+        [Test]
+        public void Test1() { MakeTest(2, 2, "4"); }
+        [Test]
+        public void Test2() { MakeTest(2, 5, "0"); }
+        [Test]
+        public void Test6() { MakeTest(10, 10, "4008004"); }
+        [Test]
+        public void Test7() { MakeTest(20, 20, "401200499400100"); }
+    }
+}
